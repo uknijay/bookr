eb81e98f989b525447e0b02ad455c79969af18c4
30002276C <3002276C@student.gla.ac.uk>
2026-03-23 19:51:14 +0000

    forms, url, views, login.html, detail.html updated for fixes + register support.

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.utils import timezone
-from .models import Event
+from .models import Event, Customer
 
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.hashers import check_password
+from django.contrib import messages
 from .decorators import *
-from main.models import EventPhoto, Account
-from .forms import LoginForm
+from main.models import EventPhoto, Account, Books
+from .forms import *
+from django.db import transaction
 
 
 ## NOTE: Look at /decorators.py, can use these to check if user is logged in and allow only logged in user to access view. 
@@ -20,6 +22,9 @@ def discover(request):
         "events": events,
     })
 
+def about(request):
+    return render(request, "main/static_pages/about.html")
+
 def contact(request):
     return render(request, "main/static_pages/contact.html")
 
@@ -28,7 +33,7 @@ def user_login(request):
         return redirect("discover")
     
     if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
 
             email = form.cleaned_data.get('email')
@@ -64,9 +69,37 @@ def user_logout(request):
     request.session.flush()
     return redirect('discover')
 
-def event_detail(request, eventId):
+def register_user(request, accountType):
+    if request.session.get('accountId'):
+        return redirect("discover")
+    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                account = form.save()
+                username = form.cleaned_data.get('username')
 
-    event =  get_object_or_404(Event, id=eventId)
+                if accountType == "business":
+                    Business.objects.create(account=account, displayName=username)
+                else:
+                    Customer.objects.create(account=account, name=username)
+
+                request.session["accountId"] = account.id
+                request.session["accountType"] = account.accountType
+                request.session["accountEmail"] = account.email
+
+                return redirect('discover')
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'main/account/register.html', {'form': form, 'accountType': accountType})
+
+
+def event_detail(request, event_id):
+
+    event =  get_object_or_404(Event, id=event_id)
     eventPhotos = EventPhoto.objects.filter(event=event)
 
 
@@ -95,16 +128,17 @@ def event_detail(request, eventId):
     })
 
 @customer_required #Decorator handles checking if user logged in and is customer, if not logged in --> /login, if not customer --> /discover
-def book_event(request, eventId):
+def book_event(request, event_id):
     if request.method != "POST":
-        return redirect("event_detail", event_id=eventId)
+        return redirect("event_detail", event_id=event_id)
 
-    event =  get_object_or_404(Event, id=eventId)
+
+    event =  get_object_or_404(Event, id=event_id)
     customer = get_object_or_404(Customer, accountId=request.session.get("accountId"))
 
     if Books.objects.filter(customerId=customer, eventId=event).exists():
         messages.warning(request, "User already booked this event")
-        return redirect("event_detail", event_id=eventId)
+        return redirect("event_detail", event_id=event_id)
 
     try:
         booking = Books(customerId=customer, eventId=event)
@@ -114,7 +148,7 @@ def book_event(request, eventId):
     except Exception as e:
         messages.error(request, str(e))
 
-    return redirect("event_detail", event_id=eventId)
+    return redirect("event_detail", event_id=event_id)
 
-def about(request):
-    return render(request, "main/static_pages/about.html")
+
+
